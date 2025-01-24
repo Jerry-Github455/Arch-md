e417527c3addf28ebf2318e29738cfd80dfba698
Jerry-Github455 <ojerindenelson@gmail.com>
2025-01-24 16:51:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
-global.botname = ["Arch Md"]
+global.owner = ["2348025053884"]
+global.botname = ["『ᏫᎮ』𝕃𝕦𝕗𝕗𝕪 ✤"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
 global.anticall = false // set true to turn anticall always on
